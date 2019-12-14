b1588fc8a6beedab1d65a2080228cdb40df84d8c
valkirium <45563827+valkirium@users.noreply.github.com>
2019-12-14 21:35:25 +0300

    Update adams method with A.cpp

--- adams method with A/adams method with A/adams method with A.cpp
+++ adams method with A/adams method with A/adams method with A.cpp
@@ -1,4 +1,4 @@
-﻿#include "iostream"
+#include "iostream"
 #include "vector"
 #include "cmath"
 
@@ -6,21 +6,21 @@ using namespace std;
 
 double F(double y, double t)
 {
-	return -t * y + (1 - t) * exp(t) * pow(y, 2);
+	return 2 * y - 4 * t * t * t + 6 * t * t - 20 * t + 10;
 }
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
 
-	double h = 0.1, e = 0.1, eps = 1, t = 0, y2;
+	double h = 0.1, e = 0.1, eps = 0.001, t = 0, y2;
 
 	vector<double> y;
 
-	
 
-	y.push_back(1);
+
 	y.push_back(0);
+	y.push_back(1 );
 
 	while (eps > e)
 	{
@@ -31,7 +31,7 @@ int main()
 
 		eps = abs(y2 - y[0]);
 
-		
+
 		y[1] = y2;
 	}
 
@@ -45,10 +45,10 @@ int main()
 
 	for (int i = 0; i < 4; i++)
 	{
-		cout << "y = " << y[i] << '\t' << 1 / (-exp(t) + 2 * exp(0.5 * pow(t, 2))) << "\t h = " << h << "\t t = " << i * h << endl;
+		cout << "y = " << y[i] << '\t' << 6 * h * i + 10 * h * i << "\t h = " << h << "\t t = " << i * h << endl;
 	}
 
-	eps = abs(y[3] + 1 / (-exp(t*3) + 2 * exp(0.5 * pow(t*3, 2))));
+	eps = abs(y[3] + 6 * h * 3 + 10 * h * 3);
 
 	for (int i = 4; t <= 1; i++)
 	{
@@ -56,11 +56,11 @@ int main()
 
 		y.push_back(y[i - 1] + (h / 24) * (55 * (F(y[i - 1], t - h)) - 59 * (F(y[i - 2], t - 2 * h)) + 37 * (F(y[i - 3], t - 3 * h)) - 9 * (F(y[i - 4], t - 4 * h))));
 
-		cout << "y = " << y[i] << '\t' << -1 / (-1 + i * h) << "\t h = " << h << "\t t = " << i * h << endl;
+		cout << "y = " << y[i] << '\t' << 6 * h * i + 10 * h * i << "\t h = " << h << "\t t = " << i * h << endl;
 
-		if (abs(y[i] + 1 / (-1 + i * h)) > eps)
+		if (abs(y[i] + 6 * h * i + 10 * h * i) > eps)
 		{
-			eps = abs(y[i] + 1 / (-exp(t*i) + 2 * exp(0.5 * pow(t*i, 2))));
+			eps = abs(y[i] + 6 * h * i + 10 * h * i);
 		}
 	}
 
